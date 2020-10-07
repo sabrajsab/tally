6fa890315fed59c2853316e2f056a076348ba6cf
Sabyasachi <43935997+sabrajsab@users.noreply.github.com>
2020-10-08 00:44:03 +0530

    Update scanPositiveInt.cpp

--- cpp_string_manipulation/scanPositiveInt.cpp
+++ cpp_string_manipulation/scanPositiveInt.cpp
@@ -1,34 +1,40 @@
-//takes input positive integer nothing else
 
 int scanPositiveInt ()
 {
-    int neg=1,val=0;
-    bool isNotInt=false;
-    register int c=getchar();
-    if(c=='-')
-    {
-        neg=-1;
-        c=getchar();
+        int          neg      = 1;
+        int          val      = 0;
+        bool         isNotInt = false;
+        register int c        = getchar ();
+    if(c == '-') {
+
+        neg = -1;
+        c = getchar ();
     }
-    do
-    {
-        if (c<48 || c>57) {
+    do {
+
+        if (c < 48 || c > 57) {
+
             isNotInt=true;
         }
-        val*=10;
-        val+=(c-48);
-    }while((c=getchar())!=10 && c !=32);
+        val *= 10;
+        val += (c - 48);
+    } while ((c = getchar()) != 10 && c != 32);
 
     if (isNotInt) {
+
         if (c == 32) {
+
             while ((getchar()) != '\n');
         }
-        printf("Please enter integer value\n");
-        return scanPositiveInt();
+
+        printf ("Please enter integer value\n");
+        return scanPositiveInt ();
     }
 
     if (neg==-1) {
+
         if (c == 32) {
+
             while ((getchar()) != '\n');
         }
         printf("please enter positive value.\n");
