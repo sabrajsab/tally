1df1eb316f8d808c3f35f5ef55fc0102e22a561f
Sabyasachi <43935997+sabrajsab@users.noreply.github.com>
2020-10-08 00:43:25 +0530

    Update scanInt.cpp

--- cpp_string_manipulation/scanInt.cpp
+++ cpp_string_manipulation/scanInt.cpp
@@ -1,28 +1,32 @@
-int scanInt()
+int scanInt ()
 {
-    int neg=1,val=0;
-    bool isNotInt=false;
-    register int c=getchar();
-    if(c=='-')
-    {
-        neg=-1;
-        c=getchar();
+        int          neg      = 1;
+        int          val      = 0;
+        bool         isNotInt = false;
+        register int c        =getchar ();
+    if (c == '-') {
+
+        neg = -1;
+        c = getchar();
     }
-    do
-    {
+    do{
+
         if (c<48 || c>57) {
-            isNotInt=true;
+
+            isNotInt = true;
         }
         val *= 10;
-        val+=(c-48);
-    }while((c=getchar())!=10 && c !=32);
+        val += (c - 48);
+    } while ((c = getchar ()) != 10 && c != 32);
 
     if (isNotInt) {
+
         if (c == 32) {
+
             while ((getchar()) != '\n');
         }
         printf("Please enter integer value\n");
-        return scanInt();
+        return scanInt ();
     }
     return val * neg;
 }
