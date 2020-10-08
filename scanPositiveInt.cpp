601141ee0cf19ba0137a394b7a96ab921a894154
Sabyasachi <43935997+sabrajsab@users.noreply.github.com>
2020-10-08 11:53:53 +0530

    Update scanPositiveInt.cpp

--- scanPositiveInt.cpp
+++ scanPositiveInt.cpp
@@ -1,24 +1,69 @@
-
-int scanPositiveInt ()
+int pow (int a, int b)
 {
-        int          neg      = 1;
-        int          val      = 0;
-        bool         isNotInt = false;
-        register int c        = getchar ();
-    if(c == '-') {
+    if (b == 0) {
+        return 1;
+    }
+    return a * pow (a, b - 1);
+}
+
+int IntMax ()
+{
+        int        noOfBits = 8 * sizeof (int);
+        static int returnVal = 0;
+    
+    if (returnVal == 0) {
+        returnVal = (pow(2, noOfBits - 1) - 1);
+    }
+    return returnVal;
+}
+
+int noOfDigits (int num)
+{
+    int nod = 0;
+    while (num) {
+
+        nod++;
+        num /= 10;
+    }
+    return nod;
+}
+
+
+int scanPositiveInt (int repeat)
+{
+
+    if (repeat >= 5) {
+
+        printf("Maximum Trials reached. Taking 0 as default input\n");
+        return 0;
+    }
+
+        int          neg           = 1;
+        int          val           = 0;
+        bool         isNotInt      = false;
+        register int c             = getchar ();
+        int          digits    = 0;
+        static   int maxNoOfDigits = 0;
+
+    if (maxNoOfDigits == 0) {
+
+        maxNoOfDigits = noOfDigits (IntMax ());
+    }
+    if (c == '-') {
 
         neg = -1;
-        c = getchar ();
+        c = getchar();
     }
     do {
 
-        if (c < 48 || c > 57) {
+        if (c<48 || c>57) {
 
-            isNotInt=true;
+            isNotInt = true;
         }
+        digits++;
         val *= 10;
         val += (c - 48);
-    } while ((c = getchar()) != 10 && c != 32);
+    } while ((c = getchar ()) != 10 && c != 32);
 
     if (isNotInt) {
 
@@ -26,20 +71,29 @@ int scanPositiveInt ()
 
             while ((getchar()) != '\n');
         }
-
-        printf ("Please enter integer value\n");
-        return scanPositiveInt ();
+        printf("Please enter integer value\n");
+        return scanPositiveInt (++repeat);
     }
 
-    if (neg==-1) {
+    if (digits >= maxNoOfDigits) {
+        if (c == 32) {
+
+            while ((getchar()) != '\n');
+        }
+        printf("No. of Digits exceeded maximum. Enter again\n");
+        return scanPositiveInt (++repeat);
+    }
+
+    if (neg == -1) {
 
         if (c == 32) {
 
             while ((getchar()) != '\n');
         }
         printf("please enter positive value.\n");
-        return scanPositiveInt();
+        return scanPositiveInt(++repeat);
     }
 
-    return val*neg;
+    return val * neg;
 }
+
