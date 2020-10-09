18586c01caafc16b7883fed472bf4fe0a781b720
Sabyasachi <43935997+sabrajsab@users.noreply.github.com>
2020-10-10 00:39:56 +0530

    Create customio.hpp

---
+++ customIO/customio.hpp
@@ -0,0 +1,26 @@
+int print (int val);
+
+int print (float val);
+
+int print (double val);
+
+int print (char val);
+
+int print (char * val);
+
+template <class T>
+T scan();
+
+int scanPositiveInt(int size = 0);
+
+int scanInt(int size = 0);
+
+float scanFloat(int size = 0);
+
+double scanDouble(int size = 0);
+
+char * scanString ();
+
+int pow (int, int);
+
+char * allocateMoreSpace (char * str, int size);
